01399183fc62f9b9afdf3f43f6f08410384600a4
toofnf <kaneva.tmr@gmail.com>
2022-10-19 00:03:56 +0300

    pseudo code

---
+++ task1/TicTacGame.py
@@ -0,0 +1,17 @@
+class TicTacGame:
+
+	def show_board():
+		pass
+
+	def validate_input():
+		pass
+
+	def start_game():
+		pass
+
+	def check_winner():
+		pass
+
+if __name__ == '__main__':
+	game = TicTac()
+	game.start_game()
