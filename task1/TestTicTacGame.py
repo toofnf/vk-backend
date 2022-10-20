f900cb358b1518408f828d341763b687848dc388
toofnf <kaneva.tmr@gmail.com>
2022-10-20 18:55:13 +0300

    unittests completely finished

--- task1/TestTicTacGame.py
+++ task1/TestTicTacGame.py
@@ -6,7 +6,7 @@ class TestTicTacGame(unittest.TestCase):
     def setUp(self):
         self.tic_tac = TicTacGame()
 
-    def test_show_board(self):
+    def test_validate_input(self):
         self.assertEqual(self.tic_tac.validate_input("5"), True)
         self.assertEqual(self.tic_tac.validate_input("111"), False)
         self.assertEqual(self.tic_tac.validate_input("0"), False)
@@ -16,6 +16,15 @@ class TestTicTacGame(unittest.TestCase):
         self.assertEqual(self.tic_tac.validate_input("a"), False)
         self.assertEqual(self.tic_tac.validate_input("___"), False)
         self.assertEqual(self.tic_tac.validate_input(False), False)
+
+    def test_pick_cell(self):
+        self.assertIn(self.tic_tac.pick_cell(), range(1, 10))
+
+    def test_check_winner(self):
+        self.assertEqual(self.tic_tac.check_winner(), False)
+
+    def test_start_game(self):
+        self.assertIn(self.tic_tac.start_game(), ["Draw!", "Xs win!", "Os win!"])
 
 
 if __name__ == '__main__':
